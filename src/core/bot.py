4e8251cf6a908d8217de1fab01dcebbd5687fe91
JEmmanuelAbishai <emmanuelabishai@gmail.com>
2026-07-22 22:07:49 +0530

    Added event functionality to the bot

--- src/core/bot.py
+++ src/core/bot.py
@@ -1,11 +1,27 @@
 import discord
 
-class client(discord.Client):
+class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("hello"):
+            await message.channel.send("Nice to meet ya I.F.")
+
+    async def on_message_edit(self, before, after):
+        if after.author == self.user:
+            return
+        if before.content == after.content:
+            return
+        await after.channel.send(
+            f" N.F.W {after.author.display_name}"
+        )
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = client(intents=intents)
-client.run('MTUyOTE3MjczNTM0NTgyMzg3NQ.GQyICl.JrOlwnkmHHLj_2E5qIwVTagDhKukY_Ekt6SUyc')
+client = Client(intents=intents)
+client.run('MTUyOTE3MjczNTM0NTgyMzg3NQ.GtwC2J.PQowDS-sdXTlSaH5hsWAwq4zH1PyVXdnKbsW1k')
